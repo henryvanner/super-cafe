e9238cbf3bf7c63684577650e3c11ce404b69a5e
henryvanner <henry.vanner@gmail.com>
2023-01-06 14:50:23 -0500

    refactor: remove redundant condition

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,6 @@ const handleGetRequest = async (req, res) => {
   try {
     const { id } = req.params
     const user = await User.findById(id)
-    if (!user) return res.status(404).end()
     return res.json(user)
   } catch (error) {
     console.log(error)
