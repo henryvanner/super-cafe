f3f8061923988af9a7e874414d33c73ca0c89376
henryvanner <henry.vanner@gmail.com>
2023-01-06 13:36:06 -0500

    fix: remove dummy middleware

--- routes/user.js
+++ routes/user.js
@@ -6,15 +6,7 @@ const validateObjectId = require('../middlewares/validateObjectId')
 const router = express.Router()
 
 router.post('/', validateUser, userControllers.handlePostRequest)
-router.get(
-  '/:id',
-  validateObjectId,
-  (req, res, next) => {
-    console.log('Extra middleware')
-    next()
-  },
-  userControllers.handleGetRequest
-)
+router.get('/:id', validateObjectId, userControllers.handleGetRequest)
 router.put('/:id', userControllers.handlePutRequest)
 router.delete('/:id', userControllers.handleDeleteRequest)
 
